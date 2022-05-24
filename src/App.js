0c927a23567da3add2413a8691e0b60293c6db34
Adhiponnuzz <adithyaminnu689693@gmail.com>
2022-05-24 18:49:51 +0530

    searchbus finished

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import './App.css';
 import Addbus from './component/Addbus';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import  'bootstrap/dist/js/bootstrap.bundle.min.js';
+import Searchbus from './component/Searchbus';
 
 function App() {
   return (
     
     <>
     <Addbus/>
+    <Searchbus/>
     
     
     </>
