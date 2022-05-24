dab5bd929fa18139f8887141997ee1889bc60391
Adhiponnuzz <adithyaminnu689693@gmail.com>
2022-05-24 19:22:55 +0530

    header finished

--- src/App.js
+++ src/App.js
@@ -4,14 +4,35 @@ import Addbus from './component/Addbus';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import  'bootstrap/dist/js/bootstrap.bundle.min.js';
 import Searchbus from './component/Searchbus';
+import Header from './component/Header';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
     
     <>
-    <Addbus/>
-    <Searchbus/>
+    <BrowserRouter>
+    <Routes>
+
+      <Route path="/" exact element={<Addbus/>}/>
+      <Route path="/search" exact element={<Searchbus/>}/>
+
+      
+
+        
+
+
+
+
+      
+
+
+    </Routes>
     
+    
+    
+    
+    </BrowserRouter>
     
     </>
 
